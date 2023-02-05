9404b6eafa82b005c89a38cedc239f21e2947eaa
Your Name <you@example.com>
2023-02-05 20:48:02 +0300

    fixed names of files

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import urllib.request
 
+# Parse Yandex schedules - list of stations
+
 BASE_URL = "https://api.rasp.yandex.net/v3.0/stations_list/?apikey=5ed2d71a-8b0e-46dd-bd83-02c1b3d30a54&format=json&lang=ru_RU"
 
 with urllib.request.urlopen(BASE_URL) as url:
